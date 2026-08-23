b1636b033a2fb308f21f15cfe2bedd642ed5e8a7
Jim DiNunzio <jim@dinunzio.com>
2026-08-22 23:53:09 -0700

    -adjusted squeeze to 0.005 because can drops out sometimes

--- dofbot_ctrl/dofbot_ctrl/graspable.py
+++ dofbot_ctrl/dofbot_ctrl/graspable.py
@@ -248,7 +248,7 @@ SODA_CAN = register(GraspableObject(
     # WHAT IS STORED IS A WIDTH, NOT THE ANGLE, because that is the coordinate
     # the rest of the model works in: grip_angle_for interpolates
     # grasp_width
-    squeeze=0.004,
+    squeeze=0.005,
 
     mesh='package://dofbot_description/meshes/cokecan.obj'))
 
